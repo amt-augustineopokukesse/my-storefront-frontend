78fb0cc5045458c184a55fac13ecbe8bfb67a871
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-05 11:35:07 +0000

    Temporary landing page links

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage: React.FC = () => {
   return (
+    <>
     <div> Landing page</div>
+    <Link to='/signup'>
+    <button>signup</button>
+    </Link>
+    <Link to='/login'>
+    <button>login</button>
+    </Link>
+    
+
+    </>
   )
 }
 
