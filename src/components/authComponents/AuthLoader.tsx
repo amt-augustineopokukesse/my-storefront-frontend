b330a38346864e36f2ae4430dcb7a404bf838b9f
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-09 22:39:09 +0000

    sign up loader added

--- src/components/authComponents/AuthLoader.tsx
+++ src/components/authComponents/AuthLoader.tsx
@@ -1,7 +1,19 @@
-import '../assets/styles/authenticationStyles/AuthLoader.scss'
+import '../../assets/styles/authenticationStyles/AuthLoader.scss';
+import { useLocation } from 'react-router-dom';
+
+const signuploader = {
+    //bottom: '0vh',
+    //position: 'relative',
+    top:'40vh',
+    left: '68vw',
+    width: '40px',
+    height: '40px'
+}
 
 export const AuthLoader: React.FC = () => {
+    const location = useLocation();
     return (
-        <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <div className="lds-spinner" style={location.pathname === '/signup' ? signuploader : {}}>
+            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     )
 }
