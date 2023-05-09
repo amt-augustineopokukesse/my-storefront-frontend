e90f9f094bf37b7c85800bcd8634adc0620b5490
john teye <john.teye@amalitech.org>
2023-05-09 11:10:11 +0000

    feat: implemented dashboard top bar with merchant profile component

---
+++ src/components/Dashboard/MerchantProfile.tsx
@@ -0,0 +1,28 @@
+import profile from '../assets/images/Ellipse 3.png'
+import { useState } from 'react';
+import '../assets/styles/dashboardStyels/MerchantProfile.scss'
+
+export const MerchantProfile: React.FC = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleClick = () => {
+        setShowMenu(!showMenu);
+    }
+    return (
+        <div className='profile-photo-menu'>
+            <img className="profile-photo" src={profile} alt="" />
+            <div className="profile-menu">
+                <button className="profile-button" onClick={handleClick}>
+                    Business Name
+                </button>
+                {showMenu && (
+                    <ul className="profile-list">
+                        <li className="profile-item"><button className="logout-button">Logout</button></li>
+                    </ul>
+                )}
+                
+            </div>
+        </div>
+    )
+}
