a9c11903d18d684962949a0c0b6583f9995bd9c8
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-10 08:40:51 +0000

    reset password loader

--- src/components/authComponents/AuthLoader.tsx
+++ src/components/authComponents/AuthLoader.tsx
@@ -28,11 +28,22 @@ const emailLoader = {
     height: '40px'
 }
 
+const passwordLoader = {
+    //bottom: '0vh',
+    //position: 'relative',
+    top:'20vh',
+    left: '17.5vw',
+    width: '40px',
+    height: '40px'
+}
+
+const resetPwId = /\/([a-zA-Z0-9$-_.+!*'(),]+)$/
+
 export const AuthLoader: React.FC = () => {
     const location = useLocation();
     return (
         <div className="lds-spinner" style={location.pathname === '/signup' ? signuploader : 
-        location.pathname === '/login' ? loginloader: location.pathname === '/resetpw1' ? emailLoader : {}}>
+        location.pathname === '/login' ? loginloader: location.pathname === '/resetpw1' ? emailLoader : location.pathname === `/resetpw2/${resetPwId}` ? passwordLoader : {}}>
             <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     )
 }
