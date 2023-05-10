179379efecf75d9600c1d191e5ca818508cafdfd
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-10 06:44:30 +0000

    email loader added

--- src/components/authComponents/AuthLoader.tsx
+++ src/components/authComponents/AuthLoader.tsx
@@ -19,11 +19,20 @@ const loginloader = {
     height: '40px'
 }
 
+const emailLoader = {
+    //bottom: '0vh',
+    //position: 'relative',
+    top:'40vh',
+    left: '17.5vw',
+    width: '40px',
+    height: '40px'
+}
+
 export const AuthLoader: React.FC = () => {
     const location = useLocation();
     return (
         <div className="lds-spinner" style={location.pathname === '/signup' ? signuploader : 
-        location.pathname === '/login' ? loginloader: {}}>
+        location.pathname === '/login' ? loginloader: location.pathname === '/resetpw1' ? emailLoader : {}}>
             <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     )
 }
