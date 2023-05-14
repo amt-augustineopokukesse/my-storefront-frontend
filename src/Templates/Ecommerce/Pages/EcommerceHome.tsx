916430437a6f08674f7e9c906b588d3ee0dd1936
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-14 10:30:20 +0000

    feat: added ecommerce menu

--- src/Templates/Ecommerce/Pages/EcommerceHome.tsx
+++ src/Templates/Ecommerce/Pages/EcommerceHome.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import Header from '../Components/Header';
 
 const EcommerceHome:React.FC = () => {
   return (
     <div>
         <Navbar />
+        <Header />
     </div>
   )
 }
