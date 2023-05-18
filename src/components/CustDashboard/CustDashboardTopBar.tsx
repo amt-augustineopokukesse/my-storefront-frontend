f13905d212719dc1435a365dbc156f5723d22b6d
john teye <john.teye@amalitech.org>
2023-05-18 20:16:03 +0000

    feat: implemented customer dashboard components

---
+++ src/components/CustDashboard/CustDashboardTopBar.tsx
@@ -0,0 +1,10 @@
+import { HeaderText } from "../HeaderText";
+import '../../assets/styles/customerDashboardStyles/CustomerDashboardTopBar.scss';
+
+export const DashboardTopBar: React.FC = () => {
+    return (
+        <div className="dashboard-top-bar">
+            <HeaderText />
+        </div>
+    )
+}
