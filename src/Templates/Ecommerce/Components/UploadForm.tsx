77b2e6fc27b519a8c03d9f7b53bc93d77ba42ad3
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-19 16:34:28 +0000

    image upload implementation

--- src/Templates/Ecommerce/Components/UploadForm.tsx
+++ src/Templates/Ecommerce/Components/UploadForm.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
-import { ProjectState } from '../../../Redux/ProjectSlice';
+import React, { ChangeEvent, useState } from 'react';
+//import { setBannerUrl } from '../../../Redux/ProjectSlice';
+//import axios from 'axios';
 
-const UploadForm: React.FC<{ project: ProjectState }> = ({ project }) => {
+const UploadForm: React.FC = () => {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  const handleBackgroundImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        // Set the image preview URL
+        setImagePreview(reader.result as string);
+      };
+
+      // Read the image file as a data URL
+      reader.readAsDataURL(file);
+      //const imageUrl  = await axios.post('https://storefrontsmes-api.amalitech-dev.net/api/merchant/upload', file);
+      
+      //dispatch(setBannerUrl(file));
+    }
+  };
+
   return (
     <div>
-        <form className="form">
-        
-        <div className='input-containers'>
-          <label className="label">Body Font Color:</label>
+      <form className="form">
+        <div className="input-containers">
+          <label className="label">Hero Background Image:</label>
           <input
-            type="color"
-            value={project.template.bodyFontColor}
-            // onChange={handleBodyFontColorChange}
-            className="color-input"
+            type="file"
+            accept="image/*"
+            onChange={handleBackgroundImageChange}
+            className="file-input"
           />
         </div>
-        </form>
+      </form>
+      {imagePreview && (
+        <div className="image-preview-container">
+          <h4>Image Preview:</h4>
+          <div className="image-preview">
+            <img src={imagePreview} alt="Preview" />
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default UploadForm;
