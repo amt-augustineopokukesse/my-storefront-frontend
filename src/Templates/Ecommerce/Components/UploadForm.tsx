1ceace7eb21e38db5a36508b66e597583f38740c
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-19 20:37:31 +0000

    successful hero background image change

--- src/Templates/Ecommerce/Components/UploadForm.tsx
+++ src/Templates/Ecommerce/Components/UploadForm.tsx
@@ -1,27 +1,43 @@
 import React, { ChangeEvent, useState } from 'react';
-//import { setBannerUrl } from '../../../Redux/ProjectSlice';
-//import axios from 'axios';
+import { setBannerUrl } from '../../../Redux/ProjectSlice';
+import axios from 'axios';
+import { useAppDispatch } from '../../../store';
+
+
+const IMAGE_UPLOAD_API_KEY = import.meta.env.VITE_IMAGE_UPLOAD_URL_KEY;
+
 
 const UploadForm: React.FC = () => {
   const [imagePreview, setImagePreview] = useState<string | null>(null);
-
+  const dispatch = useAppDispatch();
   const handleBackgroundImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
       const reader = new FileReader();
-
+  
       reader.onload = () => {
         // Set the image preview URL
         setImagePreview(reader.result as string);
       };
-
+  
       // Read the image file as a data URL
       reader.readAsDataURL(file);
-      //const imageUrl  = await axios.post('https://storefrontsmes-api.amalitech-dev.net/api/merchant/upload', file);
-      
-      //dispatch(setBannerUrl(file));
+  
+      try {
+        const formData = new FormData();
+        formData.append('key', IMAGE_UPLOAD_API_KEY);
+        formData.append('image', file);
+  
+        const response = await axios.post('https://api.imgbb.com/1/upload', formData);
+        const imageUrl = response.data.data.url;
+        console.log(imageUrl);
+        dispatch(setBannerUrl(imageUrl));
+      } catch (error) {
+        console.log('An error occurred:', error);
+      }
     }
   };
+  
 
   return (
     <div>
