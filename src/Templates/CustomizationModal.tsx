b61237c0ce067551771ac8b9d6151be10fd7a88a
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-05-19 12:49:58 +0000

    request response successful

--- src/Templates/CustomizationModal.tsx
+++ src/Templates/CustomizationModal.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import TemplateCustomizationForm from './Ecommerce/Components/ProjectCustomizationForm';
+import ProjectCustomizationForm from './Ecommerce/Components/ProjectCustomizationForm';
 import '../assets/styles/templatesStyles/Ecommerce/CustomizationModal.scss';
 
 const CustomizationModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   return (
     <div className="customization-modal">
       <div className="customization-modal-content">
-        <TemplateCustomizationForm />
+        <ProjectCustomizationForm />
         <button className="close-button" onClick={onClose}>
           Close
         </button>
