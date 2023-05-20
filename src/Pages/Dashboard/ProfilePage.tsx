ad1100af09c94fa6b9f7710e7a119309b3523698
amt-juantuah-richard <richard.juantuah@amalitech.org>
2023-05-20 18:46:30 +0000

    formik

--- src/Pages/Dashboard/ProfilePage.tsx
+++ src/Pages/Dashboard/ProfilePage.tsx
@@ -2,6 +2,9 @@ import '../../assets/styles/dashboardStyles/ProfilePage.scss'
 import profilephoto from '../../assets/images/Ellipse 15.png'
 import editLogo from '../../assets/svg/icons8-edit.svg'
 import { useEffect, useState } from 'react';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import axios from 'axios';
 
 type User = {
     [key: string]: any;
@@ -22,22 +25,28 @@ const me: EditUser = {
     editForm: {value: '', editmode: true}
 }
 
-export const ProfilePage: React.FC<EditUser> = (me) => {
+export const ProfilePage: React.FC<EditUser> = (props) => {
 
+    const { user } = props;
 
+    const [ merchantExists, setmerchantExists ] = useState(user);
 
-    const { user, editForm } = me;
 
     useEffect(() => {
         const merchant = localStorage.getItem("merchant");
         if (merchant) {
-            setmerchantExists(JSON.parse(merchant));
+            const hasMerchant = JSON.parse(merchant)
+            setmerchantExists(hasMerchant);
+            if (hasMerchant && hasMerchant.email) {
+                formik.values.email = hasMerchant.email || "";
+                formik.values.contact = hasMerchant?.contact || "";
+                formik.values.address = hasMerchant?.address || "";
+            }
         }
     }, [])
-    const [ merchantExists, setmerchantExists ] = useState(user);
     
     
-    const [ email, setEmail ] = useState({ value: merchantExists ? merchantExists.email : "", editmode: true });
+    const [ email, setEmail ] = useState({ value: "", editmode: true });
     const [ contact, setContact ] = useState({ value: "", editmode: true });
     const [ address, setAddress ] = useState({ value: "", editmode: true });
 
@@ -45,21 +54,15 @@ export const ProfilePage: React.FC<EditUser> = (me) => {
     const handleEmailChange = () => setEmail({value: email.value, editmode: email.editmode ? false : true});
     const handleContactChange = () => setContact({value: contact.value, editmode: contact.editmode ? false : true});
 
-    
-    const handleAddress = (e: React.ChangeEvent<HTMLInputElement>) => setAddress({value: e.target.value, editmode: e.target.disabled})
-    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => setEmail({value: e.target.value, editmode: e.target.disabled});
-    const handleContact = (e: React.ChangeEvent<HTMLInputElement>) => setContact({value: e.target.value, editmode: e.target.disabled});
 
-    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        
-        console.log(email.value)
-        console.log(address.value)
-        console.log(contact.value)
-        e.preventDefault();
-        // console.log(email.value)
-        // console.log(address.value)
-        // console.log(contact.value)
-    }
+    const formik = useFormik({
+        initialValues: {email: '', contact: '', address: ''},
+        onSubmit: async (values) => {
+            const vals = { ...values };
+            console.log(vals)
+            // action.resetForm();            
+        }
+    })
 
 
     return (
@@ -68,24 +71,24 @@ export const ProfilePage: React.FC<EditUser> = (me) => {
                 <img className='photo' src={profilephoto} alt="" />
                 <h3 className='name'>{ merchantExists? merchantExists.business_name : "Merchant"}</h3>
             </div>
-            <form className="form" onSubmit={handleFormSubmit}>
+            <form className="form" onSubmit={formik.handleSubmit}>
                 <div className='form-div'>
                     <label className='profile-label'>Email</label>
-                    <input disabled={email.editmode} name='email' onChange={handleEmail} value={email.value ? email.value : merchantExists ? merchantExists.email : ""} type="text"/>
+                    <input disabled={email.editmode} onChange={formik.handleChange} onBlur={handleEmailChange} type='email' name='email' value={formik.values.email}/>
                     <img className='edit-button email-edit-button' src={editLogo} alt="" onClick={handleEmailChange} />                    
                 </div>
                 <div className='form-div'>
                     <label className='profile-label'>Phone</label>
-                    <input disabled={contact.editmode} name='contact' type="text" onChange={handleContact} /**value={contact.value ? contact.value : merchantExists ? merchantExists.contact : ""}*/ />
+                    <input disabled={contact.editmode} onChange={formik.handleChange} onBlur={handleContactChange} type='tel' name='contact' value={formik.values.contact} />
                     <img className='edit-button contact-edit-button' src={editLogo} alt="" onClick={handleContactChange}/>
                 </div>
                 <div className='form-div'>
                     <label className='profile-label'>Address</label>
-                    <input disabled={address.editmode} type="text" name='address' onChange={handleAddress} /** value={address.value ? address.value : merchantExists ? merchantExists.address : ""} */ />
+                    <input disabled={address.editmode} onChange={formik.handleChange} onBlur={handleAddressChange} type='text' name='address' value={formik.values.address} />
                     <img className='edit-button location-edit-button' src={editLogo} alt="" onClick={handleAddressChange} />
                 </div>            
                 <span className='button-span'>
-                <button disabled={contact.value && email.value && address.value} type='submit' className='save-profile-details-button'>Save</button>
+                <button disabled={!formik.values.contact && !formik.values.email && !formik.values.address} type='submit' className='save-profile-details-button'>Save</button>
                 </span>
             </form>
         </div>
