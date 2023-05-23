5ef1686ad544f6e235d7e210cae96ad22aae0e57
john teye <john.teye@amalitech.org>
2023-05-23 10:15:37 +0000

    fefactor: removed unused line of code

--- src/Pages/EditTemplate/EditTemplatePage.tsx
+++ src/Pages/EditTemplate/EditTemplatePage.tsx
@@ -1,7 +1,6 @@
 import FloatingButton from "../../Templates/Ecommerce/Components/FloatingButton"
 import EcommerceHome from "../../Templates/Ecommerce/Pages/EcommerceHome";
 import { Finance } from "../../Templates/Finance/Pages/Finance";
-import { useAppSelector } from "../../store"
 
 export const EditTemplatePage: React.FC = () => {
     const template = localStorage.getItem('clickedTemplate');
