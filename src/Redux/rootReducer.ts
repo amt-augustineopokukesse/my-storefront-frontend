8a431879e417e94e4ed3afe6e8ebb2b0d32892d0
john teye <john.teye@amalitech.org>
2023-05-30 20:04:30 +0000

    feat: removed navbar from product description

--- src/Redux/rootReducer.ts
+++ src/Redux/rootReducer.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 //import authReducer from "./Authentication/authReducer";
-import authReducer from "./AuthSlice";
+import authReducer from "../Redux/Authentication/AuthSlice";
 import projectReducer from "./ProjectSlice";
 import cartReducer from "./CartSlice";
 import paymentReducer from './PaymentSlice';
